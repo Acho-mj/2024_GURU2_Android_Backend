a2666a86b215594b59aa6e7f47b029cb5c659aa9
Choi MinJung <mj020503@naver.com>
2024-07-23 10:20:33 +0900

    feat: 로그인&회원가입 구현 with JWT

---
+++ src/main/java/guru2/team8/jwt/JwtUtil.java
@@ -0,0 +1,60 @@
+package guru2.team8.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil implements InitializingBean {
+
+    @Value("${jwt.token.key}")
+    private String SECRET_KEY;
+
+    private Key key;
+
+    @Override
+    public void afterPropertiesSet() {
+        try {
+            byte[] keyBytes = java.util.Base64.getDecoder().decode(SECRET_KEY);
+            this.key = Keys.hmacShaKeyFor(keyBytes);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Invalid BASE64 encoding of SECRET_KEY", e);
+        }
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + (60 * 1000) * 30)) // 30분
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+}
